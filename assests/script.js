d9c94886af62ec9a385057e752cfbce3ab117fbd
EarlofSmith <r1smith3333@gmail.com>
2022-11-11 15:40:54 -0700

    added divs for rest of scheduler

--- assests/script.js
+++ assests/script.js
@@ -1,4 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+var currentDate = document.getElementById('currentDay')
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
